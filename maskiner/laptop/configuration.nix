99b22f04fcb7f785edabc23a1c197f05777566ef
FredzyW <fredrik@wastring.com>
2024-06-25 19:52:03 +0200

    Laptop syncthing

--- maskiner/laptop/configuration.nix
+++ maskiner/laptop/configuration.nix
@@ -18,6 +18,29 @@
 	networking.hostName = myhostname;
 
 	services.xserver.dpi = 140;
+	services = {
+		syncthing = {
+			enable = true;
+			user = "fw";
+			dataDir = "/home/fw/syncthing";
+			configDir = "/home/fw/.config/syncthing";
+			overrideDevices = true;     # overrides any devices added or deleted through the WebUI
+			overrideFolders = true;     # overrides any folders added or deleted through the WebUI
+		  guiAddress = "0.0.0.0:8384";
+			settings = {
+			  devices = {
+				"desktop" = { id = "BGEQVAA-JZM7DQB-7NFEDQ6-K7Q3HQL-P6OSNJ2-TMA3N7B-E6RP3HQ-OAMWJQL"; };
+				# "device2" = { id = "DEVICE-ID-GOES-HERE"; };
+			  };
+			  folders = {
+				"Documents" = {         # Name of folder in Syncthing, also the folder ID
+				  path = "/home/fw/docs";    # Which folder to add to Syncthing
+				  devices = [ "desktop" ];      # Which devices to share the folder with
+				};
+			  };
+			};
+		};
+	};
 	# services.spotifyd.settings.password = config.sops.secrets."spotify_password".path;
 	# https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
 	system.stateVersion = "23.11";
