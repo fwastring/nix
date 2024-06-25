9b7de0b4e01408f01067dc4ddb356231bc382e8b
FredzyW <fredrik@wastring.com>
2024-06-25 18:01:58 +0200

    Syncthin

--- maskiner/desktop/configuration.nix
+++ maskiner/desktop/configuration.nix
@@ -21,12 +21,14 @@
 	];
 networking.firewall = {
   enable = true;
-  allowedTCPPorts = [ 80 443 ];
+  allowedTCPPorts = [ 80 443 8384 22000];
   allowedUDPPortRanges = [
     { from = 4000; to = 4007; }
     { from = 8000; to = 8010; }
   ];
 };
+   networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+
 	services = {
 		openssh = {
 			enable = true;
@@ -40,6 +42,27 @@ networking.firewall = {
 			  AllowUsers fw ios
 			'';
 		};
+		syncthing = {
+			enable = true;
+			user = "fw";
+			dataDir = "/home/fw/syncthing";
+			configDir = "/home/fw/.config/syncthing";
+			overrideDevices = true;     # overrides any devices added or deleted through the WebUI
+			overrideFolders = true;     # overrides any folders added or deleted through the WebUI
+		  guiAddress = "0.0.0.0:8384";
+			settings = {
+			  devices = {
+				# "device1" = { id = "DEVICE-ID-GOES-HERE"; };
+				# "device2" = { id = "DEVICE-ID-GOES-HERE"; };
+			  };
+			  folders = {
+				"Documents" = {         # Name of folder in Syncthing, also the folder ID
+				  path = "/home/fw/docs";    # Which folder to add to Syncthing
+				  # devices = [ "device1" "device2" ];      # Which devices to share the folder with
+				};
+			  };
+			};
+	  };
 	};
 
 
