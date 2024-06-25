748672965e234ce5281bd952eceff740fcdb2f19
FredzyW <fredrik@wastring.com>
2024-06-25 10:31:03 +0200

    Fixed path to resurrect

--- shared/tmux.nix
+++ shared/tmux.nix
@@ -11,9 +11,9 @@
 		{
 			plugin = tmuxPlugins.resurrect;
 			extraConfig = ''
-				resurrect_dir=“$HOME/.tmux/resurrect”
+				resurrect_dir="$HOME/.tmux/resurrect"
 				set -g @resurrect-dir $resurrect_dir
-				set -g @resurrect-hook-post-save-all 'target=$(readlink -f $resurrect_dir/last); sed “s| --cmd .*-vim-pack-dir||g; s|/etc/profiles/per-user/$USER/bin/||g” $target | sponge $target'
+				set -g @resurrect-hook-post-save-all 'target=$(readlink -f $resurrect_dir/last); sed "s| --cmd .*-vim-pack-dir||g; s|/etc/profiles/per-user/$USER/bin/||g" $target | sponge $target'
 				set -g @resurrect-strategy-nvim 'session'
 			'';
 		}
