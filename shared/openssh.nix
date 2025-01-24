ad5bbf864fade77d2396c2db3838046b0ac93bfa
fwastring <fredrik@wastring.com>
2025-01-24 11:57:22 +0100

    Added openssh config

---
+++ shared/openssh.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ...}:
+{
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+	enable = true;
+	settings = {
+		AuthenticationMethods = "publickey password";
+		TrustedUserCAKeys = "/etc/ssh/ssh_ca.pub";
+		Banner = "none";
+		PasswordAuthentication = true;
+		# PasswordAuthentication = false;
+		PubKeyAuthentication = true;
+		Port = 22;
+	};
+  };
+}
