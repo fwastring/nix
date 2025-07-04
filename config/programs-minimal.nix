54ed0529ea157ec0cf9552d9f6a7fe337a2bd091
fwastring <fredrik@wastring.com>
2025-07-04 10:40:14 +0200

    added programs

---
+++ config/programs-minimal.nix
@@ -0,0 +1,37 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  myhostname,
+  ...
+}: {
+  	environment.systemPackages = with pkgs; [
+		# GUI
+		unstable.feishin
+		unstable.signal-desktop
+		slack
+		speedcrunch
+		remmina
+
+		# Capturing
+		byzanz
+		scrot
+
+		# Displaying
+		zathura
+		feh
+
+		# System
+		pavucontrol
+		pulseaudio
+		devour # Swallow windows
+		caligula # Burn ISOs
+
+		# Transforms
+		imagemagick
+		pandoc
+	];
+}
