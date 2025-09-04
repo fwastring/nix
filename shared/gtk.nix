6738bebeb8c9270160584125933cd78f07a9de24
fwastring <fredrik@wastring.com>
2025-09-04 09:10:09 +0200

    added hypr

--- shared/gtk.nix
+++ shared/gtk.nix
@@ -1,9 +1,19 @@
-{ pkgs, ...}:
+{ pkgs, ... }:
+let
+  bibata-modern-ice = pkgs.runCommand "bibata-modern-ice" { } ''
+    mkdir -p $out/share/icons
+    ln -s ${pkgs.fetchzip {
+      url = "https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.7/Bibata-Modern-Ice.tar.xz";
+      hash = "sha256-SG/NQd3K9DHNr9o4m49LJH+UC/a1eROUjrAQDSn3TAU=";
+    }} $out/share/icons/Bibata-Modern-Ice
+  '';
+in
 {
-	gtk = {
-		enable = true;
-		cursorTheme = {
-			name = "Bibata Modern Ice";
-		};
-	};
+  home.pointerCursor = {
+    package = bibata-modern-ice;
+    name = "Bibata-Modern-Ice";
+    size = 48;
+    gtk.enable = true;
+    x11.enable = true;
+  };
 }
