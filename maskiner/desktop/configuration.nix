3e0ee3d9330e17984f1a1b9cd9ab4669eb41e490
fwastring <fredrik@wastring.com>
2025-09-08 19:35:07 +0200

    added desktop

--- maskiner/desktop/configuration.nix
+++ maskiner/desktop/configuration.nix
@@ -16,13 +16,30 @@
     ../../moduler/users.nix
     ../../moduler/nginx.nix
     ../../moduler/k3s.nix
+    #../../moduler/lsp.nix
   ];
 
   environment.systemPackages = with pkgs; [
     unstable.lego
-	neovim
-
+k9s
+neovim
   ];
+
+  services.tailscale.enable = true;
+  services.tailscale.package = pkgs.unstable.tailscale;
+  services.vaultwarden = {
+  	enable = true;
+      dbBackend = "sqlite";
+      environmentFile = "/var/lib/vaultwarden.env";
+      config = {
+        ROCKET_ADDRESS = "127.0.0.1";
+        ROCKET_PORT = 8222;
+        DOMAIN = "https://vault.example.org";
+        SIGNUPS_ALLOWED = true;
+        ADMIN_TOKEN = "$argon2id$v=19$m=65540,t=3,p=4$...";
+        LOG_FILE = "/var/lib/bitwarden_rs/access.log";
+      };
+    };
 
   networking.hostName = myhostname;
 
